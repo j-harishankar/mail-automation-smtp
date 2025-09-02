4660ea3e9c8b3610447d12938593cc8d98b2076e
j harishankar <jharivichu27@gmail.com>
2025-09-03 01:38:10 +0530

    iss notifier completed

--- iss email automation/main.py
+++ iss email automation/main.py
@@ -1,6 +1,13 @@
 import requests
 import datetime as dt
 import smtplib
+import os 
+from dotenv import load_dotenv
+import time
+load_dotenv()
+
+MY_EMAIL = os.getenv("MY_EMAIL")
+MY_PASSWORD = os.getenv("MY_PASSWORD")
 MY_LAT = 8.539340
 MY_LONG = 76.983269
 
@@ -23,12 +30,21 @@ def is_night():
     response = requests.get("https://api.sunrise-sunset.org/json",params=parameters)
     response.raise_for_status()
     data = response.json()
-    sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0] #we only need hour so that we can compare and ensure sky is dark 
-    sunset =  data["results"]["sunset"].split("T")[1].split(":")[0]
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0]) #we only need hour so that we can compare and ensure sky is dark 
+    sunset =  int(data["results"]["sunset"].split("T")[1].split(":")[0])
     time_now = dt.datetime.now()
-    hour_now = time_now.hour
-    if time_now<=sunset and time_now>=sunrise:
+    hour_now = int(time_now.hour)
+    if hour_now>=sunset and hour_now<=sunrise:
         return True 
-
-
-    
+while True:
+    time.sleep(60)
+    if is_night() and is_iss_overhead():
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(MY_EMAIL,MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg = "subject: LOOK UP\n\nThe ISS is above your sky.",
+        )
+        
